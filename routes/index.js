c162f51a5c5e19171398be8f13e658c6124fe603
[nick_fasulo] <[nick.fasulo@codeimmersives]>
2020-03-03 15:35:54 -0500

    init

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const router = express.Router();
 
 // render home page
 router.get('/', (req, res) => {
-  res.render('main/home');
+  return res.render('main/home');
+});
+
+router.get('/logout', (req, res) => {
+  req.logOut();
+  req.session.destroy();
+  return res.redirect('/');
 });
 
 module.exports = router;
--- routes/users/controllers/userController.js
+++ routes/users/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { validationResult } = require('express-validator');
+const faker = require('faker');
 
 module.exports = {
   register: (req, res, next) => {
@@ -8,48 +9,31 @@ module.exports = {
       return res.status(422).json({ errors: errors.array() });
     }
     User.findOne({ email: req.body.email }).then(user => {
-      if (user) return console.log(`User exists`);
+      if (user) return req.flash('errors', 'User already exists');
       else {
         const newUser = new User();
 
         newUser.profile.name = req.body.name;
+        newUser.profile.picture = faker.image.avatar();
         newUser.email = req.body.email;
         newUser.password = req.body.email;
 
         newUser
           .save()
           .then(user => {
-            if (user) {
-              return res.status(200).json({ message: 'success', user });
-            }
+            req.login(user, err => {
+              if (err) {
+                return res
+                  .status(400)
+                  .json({ confirmation: false, messge: err });
+              } else {
+                res.redirect('/');
+                next();
+              }
+            });
           })
           .catch(err => next(err));
       }
     });
   }
-
-  // register: async (req, res, next) => {
-  //   const errors = validationResult(req);
-  //   const { name, email, password } = req.body;
-
-  //   if (!errors.isEmpty()) {
-  //     return res.status(422).json({ errors: errors.array() });
-  //   }
-  //   let user = await User.findOne({ email });
-
-  //   try {
-  //     if (user) {
-  //       return res.status(500).json({ message: 'User already exists' });
-  //     }
-  //     user = await User.create({
-  //       ['profile.name']: name,
-  //       email,
-  //       password
-  //     });
-
-  //     return res.json({ message: 'Success', user });
-  //   } catch (error) {
-  //     return next(error);
-  //   }
-  // }
 };
